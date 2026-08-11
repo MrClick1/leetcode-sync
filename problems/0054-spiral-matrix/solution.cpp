1f04386d9e45715e9ce1b17728ee52f154a387d5
MrClick1 <MrClick1@users.noreply.github.com>
2026-08-11 13:30:50 +0800

    0054 解答

--- problems/0054-spiral-matrix/solution.cpp
+++ problems/0054-spiral-matrix/solution.cpp
@@ -5,7 +5,43 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        // TODO: 在这里实现你的解法
-        return {};
+        int m = (int)matrix.size();     // 行数
+        int n = (int)matrix[0].size();  // 列数
+        
+        int top = 0, bottom = m-1, left = 0, right = n-1;
+
+        vector<int> result;
+
+        while (top <= bottom && left <= right) {
+            if (top > bottom || left > right) break;
+            // 向右
+            for (int j = left; j <= right; j++) {
+                result.push_back(matrix[top][j]);
+            }
+            top++;
+            
+            if (top > bottom || left > right) break;
+            // 向下
+            for (int i = top; i <= bottom; i++) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            if (top > bottom || left > right) break;
+            // 向左
+            for (int j = right; j >= left; j--) {
+                result.push_back(matrix[bottom][j]);
+            }
+            bottom--;
+            
+            if (top > bottom || left > right) break;
+            // 向上
+            for (int i = bottom; i >= top; i--) {
+                result.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+
+        return result;
     }
 };
