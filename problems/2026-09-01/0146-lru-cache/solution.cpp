ba069414aa70c1378988cf77c8a6f71408622956
MrClick1 <MrClick1@users.noreply.github.com>
2026-09-02 09:45:57 +0800

    Complete LeetCode 146 LRU cache

--- problems/2026-09-01/0146-lru-cache/solution.cpp
+++ problems/2026-09-01/0146-lru-cache/solution.cpp
@@ -2,21 +2,24 @@
 
 using namespace std;
 
-struct ListNode {
+struct DListNode {
+    int key;
     int val;
-    ListNode* next;
+    DListNode* next;
+    DListNode* pre;
 
-    ListNode(): val(0), next(nullptr) {}
-    ListNode(int _val): val(_val), next(nullptr) {}
-    ListNode(int _val, ListNode* _next): val(_val), next(_next) {}
+    DListNode(): key(0), val(0), next(nullptr), pre(nullptr) {}
+    DListNode(int _key, int _val): key(_key), val(_val), next(nullptr), pre(nullptr) {}
+    // ListNode(int _val, ListNode* _next): val(_val), next(_next) {}
 };
 
 class LRUCache {
 private:
     int size;
     int capacity;
-    unordered_map<int, ListNode*> umap;
-    ListNode* head;
+    unordered_map<int, DListNode*> umap; // [key, DListNode*]
+    DListNode* head;
+    DListNode* tail;
 
 public:
     explicit LRUCache(int capacity) {
@@ -24,27 +27,68 @@ public:
         size = 0;
         this->capacity = capacity;
 
-        head = new ListNode();
+        head = new DListNode();
+        tail = new DListNode();
+        head->next = tail;
+        tail->pre = head;
     }
 
     int get(int key) {
         // 在这里实现你的解法
         if (umap.find(key) != umap.end()) {
-            ListNode* node = umap[key];
+            DListNode* node = umap[key];
+            // 放到队头
+            removeNode(node);
             insertIntoHead(node);
-        }else {
-            return -1;
+            return node->val;
         }
+
+        return -1;
     }
 
     void put(int key, int value) {
         // 在这里实现你的解法
-        (void)key;
-        (void)value;
+        if (umap.find(key) != umap.end()) {
+            // LRU 中有 对应节点，更新节点，插入链表头，更新 umap
+            DListNode* node = umap[key];
+            node->val = value;
+            removeNode(node);
+            insertIntoHead(node);
+            umap[key] = node;
+        }else {
+            DListNode* node = new DListNode(key, value);
+
+            // 新增节点有可能会导致超过 capacity
+            if (size != capacity) {
+                // 没超过 capacity，直接插入链表头，插入 umap，size++
+                insertIntoHead(node);
+                umap[key] = node;
+                size++;
+            }else {
+                // 超过 capacity
+                // 删除尾节点
+                DListNode* removedNode = tail->pre;
+                // 删除双向链表中的尾节点，清除 umap 中的尾节点，插入并且更新 umap
+                removeNode(removedNode);
+                umap.erase(umap.find(removedNode->key));
+                insertIntoHead(node);
+                umap[key] = node;
+            }
+        }
     }
 
-    void insertIntoHead(ListNode* node) {
-
+    void insertIntoHead(DListNode* node) {
+        DListNode* nextNode = head->next;
+        head->next = node;
+        nextNode->pre = node;
+        node->pre = head;
+        node->next = nextNode;
     }
 
+    void removeNode(DListNode* node) {
+        DListNode* preNode = node->pre;
+        DListNode* nextNode = node->next;
+        preNode->next = nextNode;
+        nextNode->pre = preNode;
+    }
 };
